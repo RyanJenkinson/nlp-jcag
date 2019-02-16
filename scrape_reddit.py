d89c893d753b3253f1dce723bee0c9d421b28170
eannam <eannamorley@gmail.com>
2019-02-16 13:18:44 +0000

    Updated scraper

--- scrape_reddit.py
+++ scrape_reddit.py
@@ -12,13 +12,14 @@ subreddit = reddit.subreddit('Jokes')
 
 top_subreddit = subreddit.top(limit=1000)
 
-joke_dict = { "title":[], \
-              "body":[],
-              "score":[]}
+joke_dict = { "body":[],
+              "id":[],
+              "score":[],
+              "title":[]}
 
 for submission in top_subreddit:
     joke_dict["body"].append(submission.selftext)
-    joke_dict['id'].append(submission.id)
+    joke_dict["id"].append(submission.id)
     joke_dict["score"].append(submission.score)
     joke_dict["title"].append(submission.title)
 
