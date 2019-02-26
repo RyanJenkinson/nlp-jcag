b05d1cf3f62d9978d98139467aac6f67b3688150
Ryan Jenkinson <ryancjenkinson@gmail.com>
2019-02-26 13:51:05 +0000

    commented scraper

--- scrape_reddit.py
+++ scrape_reddit.py
@@ -4,9 +4,14 @@ import datetime as dt
 from psaw import PushshiftAPI
 
 api = PushshiftAPI()
-N = 1000
+# As of now, 1000 is the maximum limit for scraping using this API.
+scrape_limit = 1000
+
+# Create a blank dataframe which we push to a csv to store our data
 df = pd.DataFrame(columns=['body', 'id', 'score', 'title'])
 df.to_csv('data/scraped_data.csv', encoding='utf-8')
+
+# Loop over (day,month,year) in a given range
 for year in range(2012,2019):
     for month in range(1, 13):
         for day in range(1,29):
@@ -15,16 +20,17 @@ for year in range(2012,2019):
             res = list(api.search_submissions(after=start_date,
                                             subreddit='Jokes',
                                             filter=['selftext', 'id', 'score', 'title'],
-                                            limit=N,
+                                            limit=scrape_limit,
                                             score='>10',
                                             sort='asc'))
 
-            list_of_dicts = [None] * N
-            for i in range(N):
+            list_of_dicts = [None] * scrape_limit
+            for i in range(scrape_limit):
                 list_of_dicts[i] = res[i].d_
 
             df = pd.DataFrame(list_of_dicts)
             df = df[['selftext', 'id', 'score', 'title']]
             df.columns = ['body', 'id', 'score', 'title']
             time.sleep(5)
+            # Append the csv
             df.to_csv('data/scraped_data.csv', encoding='utf-8', mode = 'a', header=False)
